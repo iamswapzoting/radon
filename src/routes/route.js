faf9a74c1475ae51847bd123b67aaff8757a06bf
iamswapzoting <swapzoting123@gmail.com>
2022-06-03 03:24:14 +0530

    api assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -36,6 +36,77 @@ router.get('/candidates/:canidatesName', function(req, res){
     res.send('Done')
 })
 
+router.get('/movies', function(req,res){
+    let movies= ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    console.log(movies)
+    res.send('yessss')
+})
 
+
+router.get('/movies/:indexNumber', function(req,res){
+    let movies= ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+    let lengthOfMovies=movies.length
+    let num=((req.params))
+    let Number=movies[num.indexNumber]
+
+    if ((lengthOfMovies-1)>=num.indexNumber){
+    console.log(Number);
+    }
+    else {
+        console.log("iuse a valid index");
+    }
+    res.send('ohhhh')
+})
+
+router.get('/films',function(req,res){
+    let filmArray=[ {
+        "id" : 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+    console.log(filmArray)
+    res.send('ohhyesss')
+       
+})
+
+router.get('/films/:filmid',function(req,res){
+    let filmArray1=[ {
+        "id" : 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+
+    let lengthOfFilmArray=filmArray1.length
+    let num1=((req.params))
+    let Number1=filmArray1[num1.filmid]
+
+    if ((lengthOfFilmArray-1)>=num1.filmid){
+    console.log(Number1);
+    }
+    else {
+        console.log("No movie exists with this id");
+    }
+
+
+    
+    res.send('ohhyessssss')
+    
+})
 module.exports = router;
 // adding this comment for no reason
