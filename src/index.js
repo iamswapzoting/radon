1833db2b3ba735b3a5aa0888c900d838c0f0999f
iamswapzoting <swapzoting123@gmail.com>
2022-06-16 15:18:44 +0530

    auth2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://swapzoting:Y46OHpJkqHkkzC74@cluster0.wpcpjqt.mongodb.net/swapnil26-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,30 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
 
-    next()
-}
+const jwt= require("jsonwebtoken");
+
+const authenticate = function(req,res,next){
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+    let decodedToken = jwt.verify(token, "functionup-radon" ,function(err,data){
+      if(err){
+        return res.send({ status: false, msg: "token is invalid" });
+      } else {
+        next()
+      }
+    });
+  }
+  
 
 
 const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    let decodedToken = jwt.verify(token, 'functionup-radon')
+    let userToBeModified = req.params.userId
+    let userLoggedIn = decodedToken.userId
+
+    if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
     next()
 }
+module.exports.authenticate=authenticate
+module.exports.authorise=authorise 
